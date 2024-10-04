358d58759011a43fcb28d6b2499063e5cff9afe6
Mr-robot <hackingtest017@gmail.com>
2024-10-04 18:44:05 +0300

    fix6

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -110,23 +110,28 @@ const FilesController = {
   async getIndex(req, res) {
     try {
       const userId = req.user;
-      const { parentId = '0' } = req.query;
+      const { parentId = 0 } = req.query;
       const page = /\d+/.test((req.query.page || '').toString()) ? Number.parseInt(req.query.page, 10) : 0;
 
       const files = await dbClient.client.db().collection('files').aggregate([
         { $match: { parentId, userId: dbClient.ObjectId(userId) } },
         { $skip: parseInt(page, 10) * 20 },
         { $limit: 20 },
+        {
+          $project: {
+            _id: 0,
+            id: '$_id',
+            userId: '$userId',
+            name: '$name',
+            type: '$type',
+            isPublic: '$isPublic',
+            parentId: {
+              $cond: { if: { $eq: ['$parentId', '0'] }, then: 0, else: '$parentId' },
+            },
+          },
+        },
       ]).toArray();
-      const responseData = files.map((file) => ({
-        id: file._id,
-        userId: file.userId,
-        name: file.name,
-        type: file.type,
-        isPublic: file.isPublic,
-        parentId: file.parentId,
-      }));
-      return res.json(responseData);
+      return res.json(files);
     } catch (err) {
       return res.status(500).json({ error: 'Internal Server Error' });
     }

53f2c72dcbf5ad02421b5becb57e1d0382127ff5
Mr-robot <hackingtest017@gmail.com>
2024-10-04 17:04:44 +0300

    fix

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -6,11 +6,11 @@ import redisClient from '../utils/redis';
 const AuthController = {
   async getConnect(req, res) {
     try {
-      const auth = req.headers.authorization.replace('Basic ', '');
+      const auth = req.headers.authorization;
       if (!auth) {
         return res.status(401).json({ error: 'Unauthorized' });
       }
-      const base64ToString = Buffer.from(auth, 'base64').toString('utf-8');
+      const base64ToString = Buffer.from(auth.split(' ')[1], 'base64').toString('utf-8');
       const [email, password] = base64ToString.split(':');
 
       const isEmailExist = await dbClient.client.db().collection('users').findOne({ email, password: sha1(password) });
@@ -19,8 +19,8 @@ const AuthController = {
         return res.status(401).json({ error: 'Unauthorized' });
       }
 
-      const token = await uuid4();
-      await redisClient.set(`auth_${token}`, isEmailExist._id.toString('utf8'), 86400);
+      const token = uuid4();
+      await redisClient.set(`auth_${token}`, isEmailExist._id.toString(), 86400);
 
       return res.status(200).json({ token });
     } catch (err) {
@@ -38,10 +38,6 @@ const AuthController = {
       if (!locToken) {
         return res.status(401).json({ error: 'Unauthorized' });
       }
-      const user = await await dbClient.client.db().collection('users').findOne({ _id: dbClient.ObjectId(locToken) });
-      if (!user) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
       await redisClient.del(`auth_${token}`);
 
       return res.status(204).send();
@@ -57,7 +53,7 @@ const AuthController = {
       if (!isUserExist) {
         return res.status(401).json({ error: 'Unauthorized' });
       }
-      return res.status(200).json({ id: isUserExist._id, email: isUserExist.email });
+      return res.status(200).json(isUserExist);
     } catch (err) {
       return res.status(500).json({ error: 'Internal Server Error' });
     }
